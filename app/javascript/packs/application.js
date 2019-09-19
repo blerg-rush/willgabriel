4dfbff195ddec86fe4e4b30498ff787136b17182
TheWillGabriel <will.gabriel@gmail.com>
2019-09-19 11:29:44 -0600

    Fix font-awesome webpack import failure

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,9 +20,9 @@ import '../stylesheets/application'
 import './bootstrap_custom.js'
 
 import { library, dom } from "@fortawesome/fontawesome-svg-core"
-import fas from "@fortawesome/free-solid-svg-icons"
-import far from '@fortawesome/fontawesome-free-regular'
-import fab from '@fortawesome/fontawesome-free-brands'
+import { fas } from "@fortawesome/free-solid-svg-icons"
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
 
 library.add(fas, far, fab)
 
