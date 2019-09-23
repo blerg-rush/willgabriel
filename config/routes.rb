f20e6fb8038e1bdb7a36fad5d0f80069b97e1ce6
TheWillGabriel <will.gabriel@gmail.com>
2019-09-22 19:52:07 -0600

    Clean up posts routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/show'
-  get 'posts/new'
-  get 'posts/edit'
-  get 'posts/create'
-  get 'posts/update'
-  get 'posts/destroy'
   get '/check.txt', to: proc {[200, {}, ['it_works']]}
   root 'static#index'
   get 'static/index'
   get 'static/about'
+  resources :posts
 end
