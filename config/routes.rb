d7753e4f51c23adf0b84e397bb890cff30cf5201
TheWillGabriel <will.gabriel@gmail.com>
2019-09-22 19:37:30 -0600

    Generate basic post actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/show'
+  get 'posts/new'
+  get 'posts/edit'
+  get 'posts/create'
+  get 'posts/update'
+  get 'posts/destroy'
   get '/check.txt', to: proc {[200, {}, ['it_works']]}
   root 'static#index'
   get 'static/index'
