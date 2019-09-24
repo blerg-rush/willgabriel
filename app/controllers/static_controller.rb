5d6398908dab9dfd232eac19546cb3f8d0eba174
TheWillGabriel <will.gabriel@gmail.com>
2019-09-24 16:32:23 -0600

    Move message actions to static controller

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -3,5 +3,22 @@ class StaticController < ApplicationController
     @message = Message.new
   end
 
-  def about; end
+  def create
+    @message = Message.new(message_params)
+    if @message.valid?
+      @message.send
+      @message = Message.new
+      format.html { render :index }
+      format.js   { flash.now[:success] = "Message sent. Thank you!" }
+    else
+      format.html { render :index }
+      format.js   { flash.now[:danger] = @message.errors.full_messages.to_sentence }
+    end
+  end
+
+  private
+
+    def message_params
+      params.require(:message).permit(:name, :email, :phone_number, :body)
+    end
 end
