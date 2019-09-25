4d69c601f92125b5824a2bf6faa7ded748854499
TheWillGabriel <will.gabriel@gmail.com>
2019-09-25 12:01:00 -0600

    Ditch 'blog' routes in favor of 'posts'

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   get '/check.txt', to: proc {[200, {}, ['it_works']]}
   root 'static#index'
-  get 'blog', to: 'posts#index'
-  get 'blog/:id', to: 'posts#show'
   resources :static, only: [:index, :new, :create]
   resources :posts
 end
