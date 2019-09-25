a1170c552f9be2d549217a82e47169b4c2cead5d
TheWillGabriel <will.gabriel@gmail.com>
2019-09-25 10:49:05 -0600

    Enable hard wrap in redcarpet

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,7 +2,7 @@ module PostsHelper
   include ApplicationHelper
 
   def markdown_text(text)
-    Redcarpet::Markdown.new(MarkdownRenderer,
+    Redcarpet::Markdown.new(MarkdownRenderer.new(hard_wrap: true),
                             fenced_code_blocks: true,
                             tables: true).render(text).html_safe
   end
