1a55b0636ec1d5f48bcd4564c0d1251f341dd9ec
TheWillGabriel <will.gabriel@gmail.com>
2019-09-24 19:28:14 -0600

    Add partially-working ajax contact form

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -5,14 +5,16 @@ class StaticController < ApplicationController
 
   def create
     @message = Message.new(message_params)
-    if @message.valid?
-      @message.send
-      @message = Message.new
-      format.html { render :index }
-      format.js   { flash.now[:success] = "Message sent. Thank you!" }
-    else
-      format.html { render :index }
-      format.js   { flash.now[:danger] = @message.errors.full_messages.to_sentence }
+    respond_to do |format|
+      if @message.valid?
+        @message.send
+        @message = Message.new
+        format.html { redirect_to :index }
+        format.js   { flash.now[:success] = "Message sent. Thank you!"}
+      else
+        format.html { redirect_to :index }
+        format.js   { flash.now[:danger] = "Messaged failed. Please check your fields."}
+      end
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   get '/check.txt', to: proc {[200, {}, ['it_works']]}
   root 'static#index'
-  get 'static/index'
-  get 'static/about'
-  post 'static/create'
+  resources :static, only: [:index, :new, :create]
   resources :posts
 end
