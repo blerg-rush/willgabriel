0ce5e2362bb0de8aacf83f567dda10d658545e00
TheWillGabriel <will.gabriel@gmail.com>
2019-09-25 09:57:14 -0600

    Paginate posts index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,7 @@ require "will_paginate/array"
 class PostsController < ApplicationController
   def index
     @posts = Post.all.order(created_at: :desc)
+                 .paginate(page: params[:page], per_page: 4)
   end
 
   def show
