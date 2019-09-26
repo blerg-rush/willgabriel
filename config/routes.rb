a7711d0ea5c0d92d298b151f38e1a5421b588212
TheWillGabriel <will.gabriel@gmail.com>
2019-09-26 08:57:03 -0600

    Create specific login routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get '/check.txt', to: proc {[200, {}, ['it_works']]}
   root 'static#index'
+  get 'login', to: 'sessions#new'
+  get 'sessions', to: 'sessions#new'
   resources :static, only: [:index, :new, :create]
   resources :posts
   resources :sessions, only: [:new, :create, :destroy]
