c735a1ca0c7a85dedbbcd844569b056d3ff9e470
TheWillGabriel <will.gabriel@gmail.com>
2019-09-26 09:52:20 -0600

    Add conditional post edit/delete buttons to post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -41,6 +41,7 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    @post = Post.find(params[:id])
     @post.destroy
     flash[:info] = "Post deleted"
     redirect_to posts_url
