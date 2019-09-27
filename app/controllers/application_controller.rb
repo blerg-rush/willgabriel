834ca421fa3f3768980057921294ef7138abc6d9
TheWillGabriel <will.gabriel@gmail.com>
2019-09-26 19:50:37 -0600

    Create authentication exception handler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  ApplicationNotAuthenticated = Class.new(StandardError)
+
+  rescue_from ApplicationNotAuthenticated do
+    flash[:notice] = "Unauthorized action. Please log in."
+    redirect_to new_session_path
+  end
+
+  def authentication_required!
+    session[:current_user] || raise(ApplicationNotAuthenticated)
+  end
 end
