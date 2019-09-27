9ff30753d9704b4590436032c59427de52bd1cbd
TheWillGabriel <will.gabriel@gmail.com>
2019-09-26 19:44:50 -0600

    fill out projects actions

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,19 +1,49 @@
 class ProjectsController < ApplicationController
   def new
+    @project = Project.new
   end
 
   def create
+    @project = Project.new(project_params)
+    if @project.save
+      flash[:success] = "Project created"
+      redirect_to @project
+    else
+      flash.now[:danger] = "Invalid project details"
+      render :new
+    end
   end
 
   def show
+    @project = Project.find(params[:id])
   end
 
   def edit
+    @project = Project.find(params[:id])
   end
 
   def update
+    @project = Project.find(params[:id])
+    if @project.update(project_params)
+      flash[:success] = "Project updated"
+      redirect_to @project
+    else
+      flash.now[:danger] = "Invalid project details"
+      render :edit
+    end
   end
 
   def destroy
+    @project = Project.find(params[:id])
+    @prohect.destroy
+    flash[:info] = "Project deleted"
+    redirect_to root_path(anchor: "projects")
   end
+
+  private
+
+    def project_params
+      params.require(:project).permit(:title, :description, :url,
+                                      :source, :image, :more)
+    end
 end
