f793665b03e14a949c75b7ce61cc602900863d32
TheWillGabriel <will.gabriel@gmail.com>
2019-10-03 08:34:58 -0600

    Add devise views

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -40,6 +40,9 @@ Rails.application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
+  # Host for Devise mailer
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
